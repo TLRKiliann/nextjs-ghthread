8bdaece7791d7d1bdf56fb8eb7c6e48679dcf98e
TLRKiliann <philogenie@protonmail.com>
2023-10-17 22:16:16 +0200

    setup done

---
+++ git-thread/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Profile() {
+    return (
+        <>
+            <h1>Profile</h1>
+            <Button>Click</Button>
+        </>
+    )
+}
